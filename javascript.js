8c3574324315ab3551f9dd7c7265dcf1bfd0caa2
Wiem Gratandia Yosky <118984496+TheYosky08@users.noreply.github.com>
2022-12-15 13:34:07 +0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,155 +1,50 @@
-/* Untuk Tablet */
-@media all and (max-width: 1000px) {
-    .lesson {
-      width: 50%;
-      margin-bottom: 50px;
-    }
-    
-    .top-wrapper h1 {
-      font-size: 70px;
-      text-align: center;
-    }
+const accordion = document.getElementsByClassName("accordion");
 
-    .top-wrapper p {
-      font-size: 35px;
-      text-align: center;
-    }
+  for (let i = 0; i < accordion.length; i++) {
+    accordion[i].addEventListener('click', function () {
+      const panel = this.nextElementSibling;
 
-    .Gambar{
-      max-width: 35%;
-      display: block;
-      height: auto;
-      margin-left: auto;
-      margin-right: auto;
-    }
-    
-    .heading h2 {
-      font-size: 20px;
-    }
+      if (panel.style.display === "block") {
+        panel.style.display = "none";
+      } else {
+        panel.style.display = "block";
+      }
+    });
   }
 
-  .btn{
-    width: 50%;
+  let slideIndex = 0;
+
+const updateSlide = (n) => {
+  slideIndex += n;
+  showSlide(slideIndex);
+}
+
+const showSlide = (n) => {
+  const slides = document.getElementsByClassName("slider-item");
+  if (n > slides.length - 1) {
+    slideIndex = 0;
   }
-
-  .form-control{
-    width: 70%;
-
+  if (n < 0) {
+    slideIndex = slides.length - 1;
   }
-
-  .form-pesan{
-    width: 70%;
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
-  
-  /* Untuk Smartphone */
-  @media all and (max-width: 780px) {
-    .lesson {
-      width: 100%;
-    }
-    
-    .top-wrapper{
-      width: 100%;
-    }
+  slides[slideIndex].style.display = "block";
+}
+showSlide(slideIndex);
+
+button.addEventListener('click', function() {
+	var rs = document.forms["formInput"]["email"].value;
+	var atps=rs.indexOf("@");
+	var dots=rs.lastIndexOf(".");
+	if (atps<1 || dots<atps+2 || dots+2>=rs.length) {
+		alert("Alamat email tidak valid.");
+		return false;
+	} else {
+		alert("Pesan anda sudah terkirim.");
+	}
+})
 
 
-    .Gambar{
-      max-width: 35%;
-      display: block;
-      height: auto;
-      margin-left: auto;
-      margin-right: auto;
-    }
-    
-   label #btn{
-    display: block;
-   }
-
-   ul{
-    position: fixed;
-    width:100%;
-    margin-right: 0;
-    right: -100%;
-    top:80px;
-    background-color: rgb(10, 56, 65);
-    text-align: center;
-    transition:.5s ;
-   }
-
-   ul li{
-    display: block;
-    line-height: 10px;
-    margin: 50px 0;
-    color:black
-   }
-
-   ul li a :hover{
-    border-radius: 0px;
-   }
-
-   #check:checked ~ ul{
-    right: 0;
-   }
-  
-    #check:checked ~ label #btn{
-      display: none;
-    }
-
-    #check:checked ~ label #close{
-      display: block;
-    }
-    .top-wrapper h1 {
-      font-size: 40px;
-      line-height: 36px;
-    }
-    
-    .top-wrapper p {
-      font-size: 25px;
-      line-height: 30px;
-      margin-bottom: 10px;
-    }
-
-    .form-control{
-      width: 50%;
-    }
-
-    .form-pesan{
-      width:50%;
-    }
-
-    .btn{
-      width: 30%;
-    }
-
-    .slider-container {
-      background-color: #eee;
-      position: relative;
-      width: 100%;
-      height: 100%x;
-      border-radius: 30px;
-      padding-bottom: 0px;
-    }
-
-  .prev,
-  .next {
-    position: absolute;
-    top: 163px;
-    left: 32.%;
-    font-weight: bold;
-    font-size: 18px;
-    cursor: pointer;
-    border-radius: 0 3px 3px 0;
-    user-select: none;
-  }
-
-  .overlay .container{
-    position: relative;
-    background-color: white;
-    width: 40%;
-    height: 30%;
-    border: 3px #000000 solid;
-    border-radius: 20px;
-    padding-top: 0px 10px 0px 10px;
-  }
-
-  }
 
